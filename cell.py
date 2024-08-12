477faa5f95fe936af350fa34c06552f0463f8953
ereyesmelendez <ereyesmelendez35@gmail.com>
2024-08-12 00:33:08 -0400

    Added the functinoality to display the grid for our maze

--- cell.py
+++ cell.py
@@ -17,26 +17,13 @@ class Cell:
         self._y1 = min(y1, y2)
         self._x2 = max(x1, x2)
         self._y2 = max(y1, y2)
-        if self._win is None:
-            return
+     
+        if self.has_left_wall is True:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "black")
+        if self.has_right_wall is True:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "black")
+        if self.has_top_wall is True:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "black")
+        if self.has_bottom_wall is True:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "black")
         
-        left_wall_fill = "black" if self.has_left_wall else "white"
-        right_wall_fill = "black" if self.has_right_wall else "white"
-        top_wall_fill = "black" if self.has_top_wall else "white"
-        bottom_wall_fill = "black" if self.has_bottom_wall else "white"
-        self._win.draw_line(
-            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)),
-            left_wall_fill
-        )
-        self._win.draw_line(
-            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)),
-            right_wall_fill
-        )
-        self._win.draw_line(
-            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)),
-            top_wall_fill
-        )
-        self._win.draw_line(
-            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)),
-            bottom_wall_fill
-        )
--- main.py
+++ main.py
@@ -10,4 +10,14 @@ point2 = Point(30,40)
 line = Line(point1, point2)
 win.draw_line(line, "Red")
 
+grid = [[Cell(win) for _ in range(20)] for _ in range(20)]
+cell_size = 20
+for i, row in enumerate(grid):
+    for j, cell in enumerate(row):
+        x1=j * cell_size
+        y1 = i * cell_size
+        x2 = (j +1) * cell_size
+        y2 = (i +1) * cell_size
+        cell.draw(x1, y1, x2, y2)
+
 win.wait_for_close()# keep this at the end 
